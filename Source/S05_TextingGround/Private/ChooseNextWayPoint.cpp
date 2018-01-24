78539cca95458bef1e500e37b9bb31cdb8b50c19
DESKTOP-FCTLT5M\hoang <hoanglechanhtu01@gmail.com>
2018-01-24 20:37:38 +0700

    Composition over inheritance!

--- Source/S05_TextingGround/Private/ChooseNextWayPoint.cpp
+++ Source/S05_TextingGround/Private/ChooseNextWayPoint.cpp
@@ -4,23 +4,28 @@
 #include "Public/ChooseNextWayPoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "Public/PatrollingGuard.h" //TODO remove later
+#include "Public/PatrolRoute.h" //TODO remove later
 
 EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
 	//Get patrol points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrollPoints = PatrollingGuard->PatrolPointsCpp;
+	 
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+	auto PatrolPoints = PatrolRoute->GetPatrolRoute();
 
 	//Set next waypoint
 
+	if (PatrolPoints.Num() == 0) {
+		UE_LOG(LogTemp, Warning, TEXT("zero member of patrol points"));
+		return EBTNodeResult::Failed;
+	}
 	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	BlackBoardComp->SetValueAsObject(WaypointKey.SelectedKeyName,PatrollPoints[Index]);
+	BlackBoardComp->SetValueAsObject(WaypointKey.SelectedKeyName,PatrolPoints[Index]);
 	 
 	//Cycle index
-	auto NextIndex = (Index + 1) % PatrollPoints.Num();
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
 	BlackBoardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex); 
 	UE_LOG(LogTemp, Warning, TEXT("Index : %i"),Index);
 	return EBTNodeResult::Succeeded;
---
+++ Source/S05_TextingGround/Public/PatrolRoute.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "PatrolRoute.generated.h"
+
+/*
+A route card to help AI choose their next waypoint
+
+
+*/
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class S05_TEXTINGGROUND_API UPatrolRoute : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UPatrolRoute();
+
+	TArray<AActor*> GetPatrolRoute() const;
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditInstanceOnly, Category = Setup)
+		TArray<AActor*> PatrolPointsCpp;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+	
+};
